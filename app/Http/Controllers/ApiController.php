5cf12dada222b21eb942cce9c339f995d15d80fc
anamaria.cuevasgarcia <ana.bdr.7@gmail.com>
2024-10-16 11:39:51 +0200

    added saveCharacters and index

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use GuzzleHttp\Client;
+use App\Models\Character;
 
 class ApiController extends Controller
 {
     public function getCharacters(){
         
-        $publicKey= "2203d7f7f420afeaaa0c0c4f3ccf5640";
-        $privateKey= "6836c03be12e0510f732bedadbe28045aebe86aa";
+        $publicKey = "2203d7f7f420afeaaa0c0c4f3ccf5640";
+        $privateKey = "6836c03be12e0510f732bedadbe28045aebe86aa";
         $timestamp= time();
         $hash = md5($timestamp . $privateKey . $publicKey);
 
@@ -31,15 +32,35 @@ class ApiController extends Controller
             if ($response->getStatusCode() >= 200 && $response->getStatusCode() < 300) {
                 $data = json_decode($response->getBody(), true);
                 $characters = $data['data']['results'];
+                $this->saveCharacters($characters);
                 return response()->json($characters);
             } else {
                 file_put_contents('error.log',$response->getStatusCode(). ': ' . $e->getMessage());
-                return response()->json(['message' => 'ha ocurrido un error'],500);
+                return response()->json($this->index());
             }
         } catch (RequestException $e) {
             file_put_contents('errorCURL.log',$response->getStatusCode(). ': ' . $e->getMessage());
-            return response()->json(['message' => 'ha ocurrido un error en el curl'],500);
+            return response()->json($this->index());
         }
+    }
+
+    public function saveCharacters($data)
+    {
+        $charactersToSave = [];
+
+        foreach ($data as $character) {
+            $charactersToSave[] = [
+                'id' => $character['id'],
+                'name' => $character['name'],
+                'description' => $character['description'],
+            ];
+        }        
+        Character::upsert($charactersToSave, ['id'], ['name', 'description']);
+    }
+
+    public function index()
+    {
+        return Character::limit(10)->get();
     }
 
     
